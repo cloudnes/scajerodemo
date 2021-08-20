eaad5bd9530159cfec5cb86ad0d83134097fc1f5
cloudnes <51764110+cloudnes@users.noreply.github.com>
2021-08-20 14:20:19 -0500

    Update rundeck.sh

--- rundeck.sh
+++ rundeck.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+su ctc-admin
 KEY=$1
 sudo rpm -Uvh https://packages.microsoft.com/config/rhel/8/packages-microsoft-prod.rpm
 sudo yum -y install blobfuse
 
 sudo mkdir /mnt/resource/blobfusetmp -p
 sudo chown ctc-admin:ctc-admin /mnt/resource/blobfusetmp
-touch /home/ctc-admin/fuse_connection.cfg
+touch ~/fuse_connection.cfg
 echo "accountName ctcrndckst
 accountKey ${KEY}
-containerName rundeck" > /home/ctc-admin/fuse_connection.cfg
+containerName rundeck" > ~/fuse_connection.cfg
 
 chmod 600 ~/fuse_connection.cfg
 mkdir /home/ctc-admin/rundeck #create directory /home/ctc-admin/rundeck
